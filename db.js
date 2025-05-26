1f971e5f289828829e11eb5935569782a85a2043
iliassmn13 <145573174+iliassmn13@users.noreply.github.com>
2025-05-25 23:58:34 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'iliassmn13',
   host: 'db.ovtwdvccjotkwrndmcwn.supabase.co',
-  database: 'postgres',
+  database: 'majilias contact',
   password: 'Majdouline@2', // Mets ici le mot de passe de ta base Supabase
   port: 5432,
   ssl: { rejectUnauthorized: false }
